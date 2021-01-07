b998f9886acab94b29291e3dea3731308b9ed0dc
Andrew <kungpaogao@gmail.com>
2021-01-07 14:50:11 -0500

    Add ViewModel for dining list; bump package versions

--- app/src/main/java/org/cornelldti/flux/dininglist/DiningListFragment.kt
+++ app/src/main/java/org/cornelldti/flux/dininglist/DiningListFragment.kt
@@ -1,11 +1,13 @@
 package org.cornelldti.flux.dininglist
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import org.cornelldti.flux.R
 import org.cornelldti.flux.data.Facility
@@ -17,30 +19,20 @@ import org.cornelldti.flux.data.Facility
  */
 class DiningListFragment : Fragment(), DiningListAdapter.FacilityListener {
 
+    private lateinit var viewModel: DiningListViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
+        Log.i("DiningListFragment", "Called ViewModelProvider.get")
+        viewModel = ViewModelProvider(this).get(DiningListViewModel::class.java)
+
         // Inflate the layout for this fragment
         val rootView = inflater.inflate(R.layout.fragment_dining_list, container, false)
 
-        val data = listOf(
-            Facility("f1", "Facility One"),
-            Facility("f2", "Facility Two"),
-            Facility("f3", "Facility Two"),
-            Facility("f4", "Facility Two"),
-            Facility("f5", "Facility Two"),
-            Facility("f6", "Facility Two"),
-            Facility("f7", "Facility Two"),
-            Facility("f8", "Facility Two"),
-            Facility("f9", "Facility Two"),
-            Facility("f10", "Facility Two"),
-            Facility("f11", "Facility Two"),
-            Facility("f12", "Facility Two"),
-            Facility("f13", "Facility Two"),
-            Facility("f14", "Facility Two"),
-        )
+        val data = viewModel.data
 
         val recyclerView = rootView.findViewById<RecyclerView>(R.id.dining_list)
         recyclerView.adapter = DiningListAdapter(data, this)
---
+++ app/src/main/java/org/cornelldti/flux/dininglist/DiningListViewModel.kt
@@ -0,0 +1,39 @@
+package org.cornelldti.flux.dininglist
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import org.cornelldti.flux.data.Facility
+
+class DiningListViewModel: ViewModel() {
+    val data = listOf(
+        Facility("f1", "Facility One"),
+        Facility("f2", "Facility Two"),
+        Facility("f3", "Facility Two"),
+        Facility("f4", "Facility Two"),
+        Facility("f5", "Facility Two"),
+        Facility("f6", "Facility Two"),
+        Facility("f7", "Facility Two"),
+        Facility("f8", "Facility Two"),
+        Facility("f9", "Facility Two"),
+        Facility("f10", "Facility Two"),
+        Facility("f11", "Facility Two"),
+        Facility("f12", "Facility Two"),
+        Facility("f13", "Facility Two"),
+        Facility("f14", "Facility Two"),
+    )
+
+    init {
+        Log.i("DiningListViewModel", "DiningListViewModel created!")
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.i("DiningListViewModel", "DiningListViewModel destroyed!")
+    }
+
+    fun fetchDiningList() {
+        TODO()
+    }
+
+
+}
