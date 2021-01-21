bcdca37a425dbc33818c404539f80f967124bcd1
Andrew <kungpaogao@gmail.com>
2021-01-21 00:50:35 -0500

    Simplify main activity
    - variable is only used once so it doesn't make sense to store and init

--- app/src/main/java/org/cornelldti/flux/MainActivity.kt
+++ app/src/main/java/org/cornelldti/flux/MainActivity.kt
@@ -4,28 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.lifecycleScope
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
-
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.i("MainActivity", "Created MainActivity")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        auth = Firebase.auth
-        signInAnonymously()
-    }
 
-    private fun signInAnonymously() {
-        Log.i("MainActivity", "signInAnon:start")
+        // perform anonymous sign-in authentication
         this.lifecycleScope.launch {
-            auth.signInAnonymously().await()
+            Firebase.auth.signInAnonymously().await()
         }
     }
-
 }
