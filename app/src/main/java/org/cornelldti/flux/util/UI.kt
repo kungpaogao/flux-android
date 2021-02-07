1d14ee68bb82f60cf22372e813d3320fdf9c1dee
Andrew <kungpaogao@gmail.com>
2021-02-07 01:03:47 -0500

    Add UI util function for `getColor` and some docs
    - hopefully reduces repeated code and general verbosity
    - extension functions are beautiful

--- app/src/main/java/org/cornelldti/flux/util/UI.kt
+++ app/src/main/java/org/cornelldti/flux/util/UI.kt
@@ -1,13 +1,31 @@
 package org.cornelldti.flux.util
 
+import android.graphics.Color
 import android.util.TypedValue
 import androidx.annotation.AttrRes
+import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 
+/**
+ * Extension function for getting attribute value from `R.attr` resources
+ */
 fun Fragment.getAttr(@AttrRes attrRes: Int): Int {
     val value = TypedValue()
     requireContext().theme.resolveAttribute(attrRes, value, true)
     return TypedValue.complexToDimensionPixelSize(
         value.data, resources.displayMetrics
     )
+}
+
+/**
+ * Extension function for getting color value from `R.color` resources
+ */
+fun Fragment.getColor(@ColorRes colorRes: Int): Int {
+    return context?.let {
+        ContextCompat.getColor(
+            it,
+            colorRes
+        )
+    } ?: Color.TRANSPARENT
 }
